8b91f17987d9b753d74e0e19d3d96b18753de88c
Wurikos <50768680+Wurikos@users.noreply.github.com>
2021-01-11 13:24:08 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ffc2689b35b8a001ee220e3",
+				"5ffc2689b35b8a001ee220e5"
+			]
+		},
+		"5ffc2689b35b8a001ee220e3": {
+			"id": "5ffc2689b35b8a001ee220e3",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ffc2689b35b8a001ee220e5": {
+			"id": "5ffc2689b35b8a001ee220e5",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
